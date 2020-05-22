7b5cf043b50ee65562d754b83ef70250f0d11f95
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-05-22 19:45:20 +0200

    added Method security

---
+++ src/main/java/org/unidue/ub/unidue/almaregister/configuration/MethodSecurity.java
@@ -0,0 +1,11 @@
+package org.unidue.ub.unidue.almaregister.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        securedEnabled = true)
+public class MethodSecurity extends GlobalMethodSecurityConfiguration {
+}
