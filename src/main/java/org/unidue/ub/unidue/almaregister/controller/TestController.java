b94b3b7b2378c5a4897fea7f6265a456965a912d
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-03-12 08:16:31 +0100

    removal of unused files, adding test import

--- src/main/java/org/unidue/ub/unidue/almaregister/controller/TestController.java
+++ src/main/java/org/unidue/ub/unidue/almaregister/controller/TestController.java
@@ -5,36 +5,52 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.unidue.ub.unidue.almaregister.client.AddressWebServiceClient;
 import org.unidue.ub.unidue.almaregister.model.wsclient.ReadAddressByRegistrationnumberResponse;
 import org.unidue.ub.unidue.almaregister.service.AlmaUserService;
+import org.unidue.ub.unidue.almaregister.service.ScheduledService;
 
 @Controller
+@RequestMapping("/test")
 public class TestController {
 
     private final AddressWebServiceClient addressWebServiceClient;
+
+    private final ScheduledService scheduledService;
 
     private final Logger log = LoggerFactory.getLogger(TestController.class);
 
     private final AlmaUserService almaUserService;
 
+
+
     TestController(AddressWebServiceClient addressWebServiceClient,
-                   AlmaUserService almaUserService) {
+                   AlmaUserService almaUserService,
+                   ScheduledService scheduledService) {
         this.addressWebServiceClient = addressWebServiceClient;
         this.almaUserService = almaUserService;
+        this.scheduledService = scheduledService;
     }
 
-    @GetMapping("/test/WS")
+    @GetMapping("/WS")
     public ResponseEntity<?> testWebService(Long identifier) {
         log.info("requesting user " + identifier);
-        ReadAddressByRegistrationnumberResponse response = this.addressWebServiceClient.getAddress(identifier);
+        ReadAddressByRegistrationnumberResponse response = this.addressWebServiceClient.getAddressByMatrikel(identifier);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/test/analytics")
+    @GetMapping("/analytics")
     public ResponseEntity<?> testAnalyticsService() {
         log.info("testing user update");
         this.almaUserService.updateUserAdresses();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/import")
+    public ResponseEntity<?> testImportJob() throws Exception {
+        log.info("testing import job");
+        this.scheduledService.runImportJob();
         return ResponseEntity.ok().build();
     }
 }
