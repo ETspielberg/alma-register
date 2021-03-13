9a6fa45ef78dafd4859cd8deaea98b76f81c56b7
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-03-13 06:19:15 +0100

    remove unnecessary functions

--- src/main/java/org/unidue/ub/unidue/almaregister/repository/HisExportRepository.java
+++ src/main/java/org/unidue/ub/unidue/almaregister/repository/HisExportRepository.java
@@ -1,11 +1,9 @@
 package org.unidue.ub.unidue.almaregister.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.unidue.ub.unidue.almaregister.model.his.HisExport;
 
-import java.util.List;
 
 @Repository
 public interface HisExportRepository extends JpaRepository<HisExport, String> {
