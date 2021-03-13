c29ce5869273ae535e6713a2131a62ab26b9ac06
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-03-13 08:20:20 +0100

    refactoring day of data collection

--- src/main/java/org/unidue/ub/unidue/almaregister/service/ScheduledService.java
+++ src/main/java/org/unidue/ub/unidue/almaregister/service/ScheduledService.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -28,7 +29,12 @@ public class ScheduledService {
     public void runImportJob() throws Exception {
         JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        String filename =  LocalDate.now().format(formatter) + "-1";
+        LocalDate today = LocalDate.now();
+        if (today.getDayOfWeek()== DayOfWeek.SUNDAY)
+            today = today.minusDays(2);
+        else if (today.getDayOfWeek()== DayOfWeek.SATURDAY)
+            today = today.minusDays(1);
+        String filename =  today.format(formatter) + "-1";
         jobParametersBuilder.addString("his.filename", filename).toJobParameters();
         JobParameters jobParameters = jobParametersBuilder.toJobParameters();
         jobLauncher.run(hisJob, jobParameters);
