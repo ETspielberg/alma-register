5d8bea88faf9af1479ded6148a9d408edabfc711
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-03-14 15:53:19 +0100

    making his data import asynchroneous

--- src/main/java/org/unidue/ub/unidue/almaregister/service/ScheduledService.java
+++ src/main/java/org/unidue/ub/unidue/almaregister/service/ScheduledService.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +26,8 @@ public class ScheduledService {
         this.hisJob = hisJob;
     }
 
-    @Scheduled(cron = "0 0 2 * * *")
+    @Scheduled(cron = "0 0 23 * * *")
+    @Async("threadPoolTaskExecutor")
     public void runImportJob() throws Exception {
         JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
